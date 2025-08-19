884c86f1681553f5c9de173149276302d1d0c010
debnathshibasish75-art <debnathshibasish75@gmail.com>
2025-08-19 18:11:39 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ export default function App() {
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <h1>Shri Krishna Portfolio</h1>
       <img 
-        src="/krishna.png"   // 👈 no import needed
+        src="/krishna.png"
         alt="Shri Krishna"
         style={{ width: '300px', borderRadius: '10px' }} 
       />
